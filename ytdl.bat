d2b1e41dc74951090d4c82a99d77fe4455c4f2c1
madkracker6969 <60474634+madkracker6969@users.noreply.github.com>
2024-09-27 02:09:03 -0700

    Create ytdl.bat
    Added code to ytdl.bat

---
+++ ytdl.bat
@@ -0,0 +1,29 @@
+ECHO OFF
+CLS
+:MENU
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1 OR 2 to select your task, or 3 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Download Youtube Channel Videos
+ECHO 2 - Update Youtube-DL.exe
+ECHO 3 - EXIT
+ECHO.
+SET /P M=Type 1, 2, or 3 then press ENTER: 
+IF %M%==1 GOTO DOWNLOAD
+IF %M%==2 GOTO UPDATE
+IF %M%==3 GOTO EOF
+:DOWNLOAD
+set /p url= Paste Youtube Channel URL:
+youtube-dl -f best -ciw -o “%(title)s.%(ext)s” -v %url%
+Echo "Completed Download(s)."
+pause
+GOTO MENU
+:UPDATE
+youtube-dl -U
+Echo "Completed Update."
+pause
+GOTO MENU
+:EOF
+Exit
